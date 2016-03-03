6ce77291f45d7b0e764d4f3cb469456c36fd2ddc
Justin Schneck <jschneck@mac.com>
2016-03-03 12:21:02 -0500

    mix compiler tasks

---
+++ lib/mix/tasks/firmware.ex
@@ -0,0 +1,11 @@
+defmodule Mix.Tasks.Firmware do
+  use Mix.Task
+
+
+
+  def run(args) do
+    # Get the system path
+    # Get the toolchain path
+    # Call Bake.Adapter.Nerves.firmware/4
+  end
+end
