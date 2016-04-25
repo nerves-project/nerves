3eb361b57b31f0d336558ddb5b224c71b8f1a360
Justin Schneck <jschneck@mac.com>
2016-04-25 15:35:13 -0400

    send err to out

--- lib/mix/nerves/utils.ex
+++ lib/mix/nerves/utils.ex
@@ -4,7 +4,7 @@ defmodule Mix.Nerves.Utils do
     stream = IO.binstream(:standard_io, :line)
     Application.put_env(:porcelain, :driver, Porcelain.Driver.Basic)
     Application.ensure_started(:porcelain)
-    Porcelain.shell(cmd, in: stream, async_in: true, out: stream)
+    Porcelain.shell(cmd, in: stream, async_in: true, out: stream, err: :out)
   end
-  
+
 end
