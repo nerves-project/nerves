c05bc8c500a776d383b9de3f8e4808dfa5223729
Justin Schneck <jschneck@mac.com>
2016-05-11 16:10:41 +0200

    added application template

---
+++ archive/nerves_bootstrap/templates/new/lib/application_name.ex
@@ -0,0 +1,20 @@
+defmodule <%= application_module %> do
+  use Application
+
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec
+
+    # Define workers and child supervisors to be supervised
+    children = [
+      # worker(<%= application_module %>.Worker, [arg1, arg2, arg3]),
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: <%= application_module %>.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+end
