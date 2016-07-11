472ff44b2bb1b4cada7b267d56bb735a57eb573f
Justin Schneck <jschneck@mac.com>
2016-07-11 13:19:37 -0400

    updated deps for exrm to support 1.3.0

--- mix.exs
+++ mix.exs
@@ -23,10 +23,9 @@ defmodule Nerves.Mixfile do
 
   defp deps do
     [
-      {:exrm, "~> 1.0.4"},
+      {:exrm, "~> 1.0.6"},
       {:earmark, "~> 0.1", only: :dev},
-      {:ex_doc, "~> 0.11", only: :dev},
-      {:porcelain, "~> 2.0"}
+      {:ex_doc, "~> 0.11", only: :dev}
     ]
   end
 
