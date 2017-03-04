bef0735e0c9ed652129b2c319e0307ffa4c1b2be
Justin Schneck <jschneck@mac.com>
2017-03-04 17:05:07 -0500

    prime the default system dep to start at 0.10.0

--- bootstrap/templates/new/mix.exs
+++ bootstrap/templates/new/mix.exs
@@ -60,7 +60,7 @@ defmodule <%= app_module %>.Mixfile do
   def deps("host"), do: []
   def deps(target) do
     [{:nerves_runtime, "~> <%= runtime_vsn %>"},
-     {:"nerves_system_#{target}", ">= 0.0.0", runtime: false}]
+     {:"nerves_system_#{target}", "~> 0.10.0", runtime: false}]
   end
 
   # We do not invoke the Nerves Env when running on the Host
