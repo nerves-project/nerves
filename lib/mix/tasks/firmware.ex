280e01bcd8cf47f52a4f2b39a0a0e6648d2f5ad8
Frank Hunleth <fhunleth@troodon-software.com>
2017-06-16 00:15:04 -0400

    Propogate mix.exs configuration to .fw metadata (#133)

--- lib/mix/tasks/firmware.ex
+++ lib/mix/tasks/firmware.ex
@@ -102,8 +102,19 @@ defmodule Mix.Tasks.Firmware do
       |> Path.join("rel/#{otp_app}")
     output = [release_path]
     args = args ++ fwup_conf ++ rootfs_additions ++ fw ++ output
+    env = standard_fwup_variables(config)
 
-    shell(cmd, args)
+    shell(cmd, args, env: env)
     |> result
+  end
+
+  defp standard_fwup_variables(config) do
+    # Assuming the fwup.conf file respects these variable like the official
+    # systems do, this will set the .fw metadata to what's in the mix.exs.
+    [{"NERVES_FW_VERSION", config[:version]},
+     {"NERVES_FW_PRODUCT", config[:name]},
+     {"NERVES_FW_DESCRIPTION", config[:description]},
+     {"NERVES_FW_AUTHOR", config[:author]},
+    ]
   end
 end
