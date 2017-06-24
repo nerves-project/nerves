8a9b4d39895c258a83dc7172347b72de5641ea53
Justin Schneck <jschneck@mac.com>
2017-06-24 06:43:24 -0700

    require packages to be passed to clean

--- bootstrap/lib/mix/tasks/nerves/clean.ex
+++ bootstrap/lib/mix/tasks/nerves/clean.ex
@@ -1,15 +1,46 @@
 defmodule Mix.Tasks.Nerves.Clean do
   use Mix.Task
+  import Mix.Nerves.IO
 
   @shortdoc "Clean artifacts for a Nerves package"
 
-  @switches [package: :string]
+  @moduledoc """
+  Cleans the build artifacts and fetch sources from given dependencies
+
+  Since this is a destructive action, cleaning of dependencies
+  only occurs when passing arguments/options:
+    * `dep1 dep2` - the names of Nerves dependencies to be cleaned separated by a space
+    * `--all` - cleans all Nerves dependencies
+  """
+
+  @switches [all: :boolean]
 
   def run(argv) do
-    {opts, _, _} = OptionParser.parse(argv, switches: @switches)
-    package = opts[:package]
+    debug_info "Clean Start"
+    {opts, packages, _} = OptionParser.parse(argv, switches: @switches)
     Mix.Tasks.Nerves.Env.run([])
-    Nerves.Env.clean(package)
+
+    packages =
+      case packages do
+        [] ->
+          if opts[:all] do
+            Nerves.Env.packages
+          else
+            Mix.raise """
+            You must specify the Nerves dependencies to clean seperated by spaces
+            Example:
+              mix nerves.clean nerves_system_rpi3
+            Or by passing --all
+              mix nerves.clean --all
+            """
+          end
+        packages ->
+          packages
+          |> Enum.map(&String.to_existing_atom/1)
+          |> Enum.map(&Nerves.Env.package/1)
+      end
+      debug_info "Clean End"
+    Nerves.Env.clean(packages)
   end
 
 end
