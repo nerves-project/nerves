6f50fe912d26b7286db70481ccca48aec3c6a5e2
Justin Schneck <jschneck@mac.com>
2017-06-27 21:39:05 -0400

    implement docker version parsing via regex.

--- test/nerves/provider/docker_test.exs
+++ test/nerves/provider/docker_test.exs
@@ -1,6 +1,8 @@
 defmodule Nerves.Provider.DockerTest do
   use NervesTest.Case, async: false
 
+  alias Nerves.Package.Providers.Docker
+
   test "Generated docker container Id is valid" do
     id = Nerves.Utils.random_alpha_num(16)
     assert Regex.match?(~r/^[a-zA-Z0-9_]*$/, id)
@@ -10,5 +12,11 @@ defmodule Nerves.Provider.DockerTest do
     id1 = Nerves.Utils.random_alpha_num(16)
     id2 = Nerves.Utils.random_alpha_num(16)
     refute id1 == id2
+  end
+
+  test "parse versions with leading zeros" do
+    assert {:ok, _} = Docker.parse_docker_version("17.0.1")
+    assert {:ok, _} = Docker.parse_docker_version("17.03.1")
+    assert {:ok, _} = Docker.parse_docker_version("0017000.00300.00001000-ce")
   end
 end
