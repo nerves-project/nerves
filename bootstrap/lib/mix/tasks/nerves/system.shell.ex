5147f9718f173810012771d6532f83c44626120d
Greg Mefford <greg@gregmefford.com>
2017-07-25 08:40:00 -0400

    More preflight checks for nerves.system.shell task

--- bootstrap/lib/mix/tasks/nerves/system.shell.ex
+++ bootstrap/lib/mix/tasks/nerves/system.shell.ex
@@ -16,24 +16,43 @@ defmodule Mix.Tasks.Nerves.System.Shell do
 
   """
 
+  @no_nerves_dep_error """
+  Nerves dependency not found in current Mix project.
+  Make sure you run this task from a Nerves-based project or the source directory of a custom Nerves system.
+  """
+
+  @no_system_pkg_error """
+  Unable to locate a relevant Nerves system package.
+  Make sure you run this task from a Nerves-based project or the source directory of a custom Nerves system.
+  """
+
+  @doc false
   def run(_argv) do
-
-    # TODO: Make sure the current project has a nerves dependency
-
-    # Don't pre-compile so that we can configure the system before building it
-    # for the first time.
-    Mix.Task.run("nerves.env", ["--disable"])
 
     # We unregister :user so that the process currently holding fd 0 (stdin)
     # can't send an error message to the console when we steal it.
     user = Process.whereis(:user)
     Process.unregister(:user)
 
+    # Start disabled so that we can configure the system before building it
+    # for the first time.
+    try do
+      Mix.Task.run("nerves.env", ["--disable"])
+    rescue
+      e in Mix.Error ->
+        if String.starts_with?(e.message, "Unknown dependency nerves for environment") do
+          Mix.raise(@no_nerves_dep_error)
+        end
+    end
+
+    pkg = Nerves.Env.system()
+    if is_nil(pkg), do: Mix.raise(@no_system_pkg_error)
+
     provider = case :os.type do
       {_, :linux} -> Nerves.Package.Providers.Local
       _ -> Nerves.Package.Providers.Docker
     end
-    provider.system_shell(Nerves.Env.system())
+    provider.system_shell(pkg)
 
     # Set :user back to the real one
     Process.register(user, :user)
