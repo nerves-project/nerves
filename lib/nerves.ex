cc01844a16f3e7d94e8f90654cdb08abd1799f10
Greg Mefford <greg@gregmefford.com>
2017-11-18 22:49:01 -0500

    Stringify the Mix.env before passing it to Path.join

--- lib/nerves.ex
+++ lib/nerves.ex
@@ -7,7 +7,7 @@ defmodule Nerves do
       profile =
         release.profile
         |> Map.put(:dev_mode, false)
-        |> Map.put(:output_dir, Path.join([project_config[:build_path], Mix.env, "rel"]))
+        |> Map.put(:output_dir, Path.join([project_config[:build_path], to_string(Mix.env), "rel"]))
         |> Map.put(:include_src, false)
         |> Map.put(:include_erts, System.get_env("ERL_LIB_DIR"))
         |> Map.put(:include_system_libs, System.get_env("ERL_SYSTEM_LIB_DIR"))
