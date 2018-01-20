0b518d5da6929c38e253ce14fd1dbe59db9251d3
Justin Schneck <jschneck@mac.com>
2018-01-20 15:14:57 -0500

    tidy up file utils

--- lib/nerves/utils/file.ex
+++ lib/nerves/utils/file.ex
@@ -3,20 +3,21 @@ defmodule Nerves.Utils.File do
   def untar(file, destination \\ nil) do
     destination = destination || File.cwd!
     System.cmd("tar", ["xf", file, "--strip-components=1", "-C", destination])
+    |> result()
   end
 
   def validate(file) do
-    cmd = 
-      Path.extname(file)
-      |> ext_cmd()
-    case System.cmd(cmd, ["-t", file]) do
-      {"", 0} -> :ok
-      {reason, _} -> {:error, reason}
-    end
+    Path.extname(file)
+    |> ext_cmd()
+    |> System.cmd(["-t", file])
+    |> result()
   end
 
   def ext_cmd(".xz"), do: "xz"
-  def ext_cmd(".gz"), do: "gz"
+  def ext_cmd(".gz"), do: "gzip"
   def ext_cmd(".tar"), do: "tar"
+
+  defp result({"", 0}), do: :ok
+  defp result({reason, _}), do: {:error, reason}
 
 end
