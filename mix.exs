9539e868902ca9c4be672eaf300c4267755cd93b
Wojtek Mach <wojtekmach@users.noreply.github.com>
2018-03-20 12:56:23 +0100

    Include CHANGELOG.md in Hex package

--- mix.exs
+++ mix.exs
@@ -67,7 +67,7 @@ defmodule Nerves.Mixfile do
   defp package do
     [
       maintainers: ["Justin Schneck", "Frank Hunleth", "Greg Mefford"],
-      files: ["lib", "LICENSE", "mix.exs", "README.md", "template", "scripts", "priv"],
+      files: ["CHANGELOG.md", "lib", "LICENSE", "mix.exs", "README.md", "template", "scripts", "priv"],
       licenses: ["Apache 2.0"],
       links: %{"Github" => "https://github.com/nerves-project/nerves"}
     ]
