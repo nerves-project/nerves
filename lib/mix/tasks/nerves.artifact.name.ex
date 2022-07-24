99538a4fa3dd2fb7db439579d218212105de6013
Udo Schneider <Udo.Schneider@homeaddress.de>
2022-07-24 14:52:53 +0200

    Add mix task to display artifact name/information
    Fixes #771
    
    This fix adds a mix task `mix nerves.artifact.name` which display various artifact  names/information w/o triggering a artifact re-build.
    
    Example:
    ```
    $ mix nerves.artifact.name
    ==> nerves
    ==> nerves_system_bbb
    Generated nerves_system_bbb app
    
    Nerves environment
      MIX_TARGET:   target
      MIX_ENV:      dev
    
    Name:               nerves_system_bbb-portable-2.15.1
    Download Name:      nerves_system_bbb-portable-2.15.1-0C8DD7C
    Download File Name: nerves_system_bbb-portable-2.15.1-0C8DD7C.tar.gz
    Download Path:      /home/udos/.nerves/dl/nerves_system_bbb-portable-2.15.1-0C8DD7C.tar.gz
    Checksum:           0C8DD7CEB69659D7486BBA6B22AE1CB498EF3487A5369411F0C157477F8FF2D0
    ```

---
+++ lib/mix/tasks/nerves.artifact.name.ex
@@ -0,0 +1,61 @@
+defmodule Mix.Tasks.Nerves.Artifact.Name do
+  @shortdoc "Prints Nerves artifact name/information"
+  @moduledoc """
+  Prints Nerves artifact name/information.
+
+  This displays various artifact names/information.
+
+  ## Examples
+
+      $ mix nerves.artifact.name nerves_system_rpi0
+
+  If the command is called without the package name,
+  `Nerves.Project.config()[:app]` will be used by default.
+  """
+  use Mix.Task
+
+  import Mix.Nerves.IO
+
+  require Logger
+
+  @recursive true
+
+  @impl Mix.Task
+  def run(argv) do
+    # We need to make sure the Nerves env has been set up.
+    # This allows the task to be called from a project level that
+    # does not include aliases to nerves_bootstrap
+    Mix.Task.run("nerves.precompile", [])
+
+    {package_name, _argv} =
+      case argv do
+        [] ->
+          {to_string(Mix.Project.config()[:app]), argv}
+
+        ["-" <> _arg | _] ->
+          {to_string(Mix.Project.config()[:app]), argv}
+
+        [package_name | argv] ->
+          {package_name, argv}
+      end
+
+    debug_info("Nerves.Artifact.Name start")
+
+    package_name = String.to_atom(package_name)
+
+    package = Nerves.Env.package(package_name)
+
+    _ =
+      if is_nil(package) do
+        Mix.raise("Could not find Nerves package #{package_name} in env")
+      else
+        Mix.shell().info("Name:               #{Nerves.Artifact.name(package)}")
+        Mix.shell().info("Download Name:      #{Nerves.Artifact.download_name(package)}")
+        Mix.shell().info("Download File Name: #{Nerves.Artifact.download_name(package)}#{Nerves.Artifact.ext(package)}")
+        Mix.shell().info("Download Path:      #{Nerves.Artifact.download_path(package)}")
+        Mix.shell().info("Checksum:           #{Nerves.Artifact.checksum(package)}")
+      end
+
+    debug_info("Nerves.Artifact.Name end")
+  end
+end
