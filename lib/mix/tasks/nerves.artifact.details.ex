f3b617fb29ec728e432ff2c9b5ab71a3e346f2f1
Udo Schneider <Udo.Schneider@homeaddress.de>
2022-07-29 15:36:05 +0200

    refactor: Renamed task to `nerves.articfact.details`

--- lib/mix/tasks/nerves.artifact.details.ex
+++ lib/mix/tasks/nerves.artifact.details.ex
@@ -1,13 +1,13 @@
-defmodule Mix.Tasks.Nerves.Artifact.Name do
-  @shortdoc "Prints Nerves artifact name/information"
+defmodule Mix.Tasks.Nerves.Artifact.Details do
+  @shortdoc "Prints Nerves artifact details"
   @moduledoc """
-  Prints Nerves artifact name/information.
+  Prints Nerves artifact details.
 
   This displays various artifact names/information.
 
   ## Examples
 
-      $ mix nerves.artifact.name nerves_system_rpi0
+      $ mix nerves.artifact.details nerves_system_rpi0
 
   If the command is called without the package name,
   `Nerves.Project.config()[:app]` will be used by default.
@@ -39,7 +39,7 @@ defmodule Mix.Tasks.Nerves.Artifact.Name do
           {package_name, argv}
       end
 
-    debug_info("Nerves.Artifact.Name start")
+    debug_info("Nerves.Artifact.Details start")
 
     package_name = String.to_atom(package_name)
 
@@ -51,11 +51,15 @@ defmodule Mix.Tasks.Nerves.Artifact.Name do
       else
         Mix.shell().info("Name:               #{Nerves.Artifact.name(package)}")
         Mix.shell().info("Download Name:      #{Nerves.Artifact.download_name(package)}")
-        Mix.shell().info("Download File Name: #{Nerves.Artifact.download_name(package)}#{Nerves.Artifact.ext(package)}")
+
+        Mix.shell().info(
+          "Download File Name: #{Nerves.Artifact.download_name(package)}#{Nerves.Artifact.ext(package)}"
+        )
+
         Mix.shell().info("Download Path:      #{Nerves.Artifact.download_path(package)}")
         Mix.shell().info("Checksum:           #{Nerves.Artifact.checksum(package)}")
       end
 
-    debug_info("Nerves.Artifact.Name end")
+    debug_info("Nerves.Artifact.Details end")
   end
 end
