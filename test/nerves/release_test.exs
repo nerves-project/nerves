780dfdb46b976e32598f4f7cb110d9e47d1d8a4e
Jon Carstens <jjcarstens@me.com>
2022-09-04 20:00:41 -0600

    Adjust release test to better support version changes
    Asserting a specific generated priorities is brittle as it drastically
    changes with each Elixir version. The ordering is also currently suboptimal.
    
    For now, just check the file begins with the consolidated protocols
    until the ordering is optimized and a better test can be constructed

--- test/nerves/release_test.exs
+++ test/nerves/release_test.exs
@@ -9,23 +9,22 @@ defmodule Nerves.ReleaseTest do
       {_, 0} = System.cmd("mix", ["deps.get"], env: [{"MIX_ENV", "#{Mix.env()}"}])
       {_, 0} = System.cmd("mix", ["release"], env: [{"MIX_ENV", "#{Mix.env()}"}])
 
-      generated =
-        Path.join(Mix.Project.build_path(), "nerves/rootfs.priorities")
-        |> only_basenames()
+      expected = """
+      srv/erlang/releases/0.1.0/consolidated/Elixir.String.Chars.beam 32000
+      srv/erlang/releases/0.1.0/consolidated/Elixir.List.Chars.beam 31999
+      srv/erlang/releases/0.1.0/consolidated/Elixir.Inspect.beam 31998
+      srv/erlang/releases/0.1.0/consolidated/Elixir.Enumerable.beam 31997
+      srv/erlang/releases/0.1.0/consolidated/Elixir.Collectable.beam 31996
+      """
 
-      expected = only_basenames("expected.rootfs.priorities")
-
-      # This is only half a test since it just considers file names
-      # and not full paths, but better than nothing
-      assert generated == expected
+      # TODO: Adjust this test to better check ordering
+      # Asserting a specific generated priorities is brittle as it drastically
+      # changes with each Elixir version. The ordering is also currently suboptimal.
+      # For now, just check the file begins with the consolidated protocols
+      # until the ordering is optimized and a better test can be constructed
+      assert Path.join(Mix.Project.build_path(), "nerves/rootfs.priorities")
+             |> File.read!()
+             |> String.starts_with?(expected)
     end)
-  end
-
-  defp only_basenames(path) do
-    str = File.read!(path)
-
-    for line <- String.split(str, "\n", trim: true),
-        [p, n] = String.split(line, " "),
-        do: [Path.basename(p), n]
   end
 end
