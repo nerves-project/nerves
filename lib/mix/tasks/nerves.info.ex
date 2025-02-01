c498514167bde3f30348b675df6921853d3e03c8
Frank Hunleth <fhunleth@troodon-software.com>
2025-02-01 14:48:07 -0500

    Remove old way of changing the Mix target in mix
    This really had questionable use expecially when it would be more
    realistic getting Nerves information with specifying the target the
    normal way.

--- lib/mix/tasks/nerves.info.ex
+++ lib/mix/tasks/nerves.info.ex
@@ -15,22 +15,16 @@ defmodule Mix.Tasks.Nerves.Info do
   use Mix.Task
   import Mix.Nerves.IO
 
-  @switches [target: :string]
-
   @impl Mix.Task
-  def run(argv) do
+  def run(_args) do
     debug_info("Info Start")
-    {opts, _, _} = OptionParser.parse(argv, switches: @switches)
     Nerves.Env.disable()
-
-    if target = opts[:target] do
-      Nerves.Env.change_target(target)
-    end
 
     Mix.Tasks.Nerves.Env.run(["--info", "--disable"])
     Mix.shell().info("Nerves:           #{Nerves.version()}")
     Mix.shell().info("Nerves Bootstrap: #{Nerves.Bootstrap.version()}")
     Mix.shell().info("Elixir:           #{System.version()}")
+
     Nerves.Env.enable()
     debug_info("Info End")
   end
