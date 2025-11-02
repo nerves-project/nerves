f3bf243b93d2832f9bf6f0e426319d5a12295463
Frank Hunleth <fhunleth@troodon-software.com>
2025-11-01 21:56:19 -0400

    Drop support for Elixir 1.14 due to CI errors

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Nerves.MixProject do
     [
       app: :nerves,
       version: @version,
-      elixir: "~> 1.14.0 or ~> 1.15.1 or ~> 1.16.0 or ~> 1.17.0 or ~> 1.18.0 or ~> 1.19.0",
+      elixir: "~> 1.15.1 or ~> 1.16.0 or ~> 1.17.0 or ~> 1.18.0 or ~> 1.19.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
       description: description(),
