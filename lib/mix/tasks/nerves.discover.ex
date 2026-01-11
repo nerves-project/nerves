8dd8d5446535948e1558bd07284b97d94754fdce
Frank Hunleth <fhunleth@troodon-software.com>
2026-01-11 10:07:04 -0500

    Add mix task for discovering Nerves devices
    This works by using the operating system's mDNS stack or a generic
    OTP-only one for querying the network. The OS stack is preferred since
    it can report back cached entries more quickly and recent versions of
    macOS prevent user applications from using mDNS directly.
    
    Here's an example run:
    
    ```
    $ mix nerves.discover
    Discovering Nerves devices (waiting up to 5000ms)...
    
    NAME         IP             SERIAL            VERSION  PRODUCT     PLATFORM  UUID
    nerves-0316  192.168.7.128  55e77bfdd5030316  0.2.1    kiosk_demo  rpi5      cbf8c271-2673-51f7-ab08-3de09af404eb
    nerves-8465  192.168.7.48
    ```
    
    The first device advertises firmware attributes which is currently an
    opt-in feature. The second device is found via a heuristic that looks
    for the ssh service on devices with hostnames that start with "nerves".
    
    Most logic is in the `nerves_discovery` library so that it can be used
    in other programs too.
    
    Currently not all fields are shown in the table since the rows can get
    really long. The UUID get elided in the current implementation when the
    terminal width is too small.

---
+++ lib/mix/tasks/nerves.discover.ex
@@ -0,0 +1,83 @@
+# SPDX-FileCopyrightText: 2026 Frank Hunleth
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+defmodule Mix.Tasks.Nerves.Discover do
+  @shortdoc "Discover Nerves devices on the local network"
+
+  @moduledoc """
+  Discover Nerves devices on the local network using mDNS
+
+  This task scans the local network for Nerves devices and displays
+  their information in a table format.
+
+  Linux users: Install `avahi-utils` for faster responses.
+
+  ## Examples
+
+      $ mix nerves.discover
+
+      $ mix nerves.discover --timeout 10000
+
+  ## Options
+
+    * `--timeout` - timeout in milliseconds to wait for replies (default 5000)
+  """
+  use Mix.Task
+
+  @switches [timeout: :integer]
+
+  @impl Mix.Task
+  def run(argv) do
+    {opts, _, _} = OptionParser.parse(argv, switches: @switches)
+    timeout = Keyword.get(opts, :timeout, 5000)
+
+    Mix.shell().info("\nDiscovering Nerves devices (waiting up to #{timeout}ms)...")
+
+    devices = NervesDiscovery.discover(timeout: timeout)
+
+    Mix.shell().info("")
+
+    if Enum.empty?(devices) do
+      Mix.shell().info("No devices found.")
+    else
+      Tablet.puts(devices, tablet_options(devices))
+    end
+  end
+
+  defp tablet_options(devices) do
+    base_columns = [:name, :ip]
+
+    optional_columns = [
+      :serial,
+      :version,
+      :product,
+      :platform,
+      :uuid
+    ]
+
+    all_keys =
+      Enum.reduce(devices, MapSet.new(), fn device, set ->
+        set_put_many(set, non_empty_keys(device))
+      end)
+
+    # See which of the optional columns are actually present
+    additional_columns =
+      Enum.filter(optional_columns, &MapSet.member?(all_keys, &1))
+
+    [
+      keys: base_columns ++ additional_columns,
+      formatter: &formatter/2,
+      column_widths: %{uuid: :expand}
+    ]
+  end
+
+  defp non_empty_keys(map) do
+    Enum.reduce(map, [], fn {k, v}, acc -> if v != nil and v != "", do: [k | acc], else: acc end)
+  end
+
+  defp set_put_many(set, values), do: Enum.reduce(values, set, &MapSet.put(&2, &1))
+
+  defp formatter(:__header__, key), do: {:ok, key |> to_string() |> String.upcase()}
+  defp formatter(_, _), do: :default
+end
